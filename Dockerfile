00ae47da17d77be8b3c593098ef1306d7fded3da
David <davidventura27@gmail.com>
2017-08-17 10:45:31 -0300

    add templating

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM debian:stretch
-RUN apt-get update && apt-get -y --no-install-recommends install python3 python3-wheel python3-pip python3-setuptools markdown
-RUN pip3 install tinys3
+RUN apt-get update && apt-get -y --no-install-recommends install python3 python3-wheel python3-pip python3-setuptools python3-jinja2
+RUN pip3 install tinys3 markdown2 pygments
 WORKDIR "/root/"
+# RUN mkdir -p html && pygmentize -S default -f html > html/style.css
 ENTRYPOINT /usr/bin/python3 /root/scripts/test.py
--- run.sh
+++ run.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
+set -eu
+
+if [ $# -ne 1 ]; then
+    echo "Usage: 1 argument, target blog root"
+    exit 1
+fi
+
 source credentials
 COMMIT=$(git rev-parse HEAD)
+TARGET="$1"
+
+if [ ! -d "$PWD/blog/raw/$TARGET" ]; then
+    echo "Target path does not exist"
+    exit 1
+fi
+
 docker run -v "$PWD/scripts":/root/scripts/ \
-           -v "$PWD/blog/raw/":/root/raw \
+           -v "$PWD/blog/raw/$TARGET":/root/target \
            -v "$PWD/blog/images":/root/images \
+           -v "$PWD/blog/template":/root/template \
            -v "$PWD/blog/html":/root/html \
            -e S3_ACCESS_KEY="$S3_ACCESS_KEY" \
            -e S3_SECRET_KEY="$S3_SECRET_KEY" \
