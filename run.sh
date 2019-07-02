b73530c5e7f6e090e50101f5a17327f730280bc3
david <davidventura27@gmail.com>
2019-07-02 21:32:44 +0200

    adapt dockerfile to put images and videos within the html dir

--- run.sh
+++ run.sh
@@ -7,10 +7,9 @@ docker build -t blogging .
 docker run \
            -v "$PWD/scripts":/root/scripts/ \
            -v "$PWD/blog/raw/":/root/raw \
-           -v "$PWD/blog/images":/root/images \
-           -v "$PWD/blog/videos":/root/videos \
+           -v "$PWD/blog/html/images":/root/images \
+           -v "$PWD/blog/html/videos":/root/videos \
            -v "$PWD/blog/template":/root/template \
            -v "$PWD/blog/html":/root/html \
-           -v "$PWD/blog/tags":/root/tags \
            -e COMMIT="$COMMIT" \
            blogging:latest
