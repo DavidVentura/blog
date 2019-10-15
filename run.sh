c329d681c2737a859cf2b0dae62b3b2327c3caa7
david <davidventura27@gmail.com>
2019-10-15 11:15:17 +0200

    remove useless copy

--- run.sh
+++ run.sh
@@ -6,4 +6,3 @@ python generate.py
 dot blog/raw/bookworm/architecture.dot -Tpng > blog/html/images/bookworm-architecture.png
 
 rsync -ar blog/html/ root@web.labs:/var/www/blog-devops
-scp _blogs-i-follow.html root@web.labs:/var/www/blog-devops/blogs-i-follow.html
