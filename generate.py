e5378c2713b4b8f8c6d40a6d450aa74cbe1a725d
david <davidventura27@gmail.com>
2021-01-07 12:02:37 +0100

    Convert dict to dataclass

--- generate.py
+++ generate.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
 import glob
 import json
-import markdown2
 import os
-import pytz
 import re
 import shutil
 import sys
 
+import markdown2
+import pytz
+
+from dataclasses import asdict, dataclass
+from datetime import datetime, date
 from pathlib import Path
-from datetime import datetime
-from jinja2 import Template
-from feedgen.feed import FeedGenerator
+from typing import Optional, List
+
 from bs4 import BeautifulSoup
+from feedgen.feed import FeedGenerator
+from jinja2 import Template
 
 BLOG_URL = 'https://blog.davidventura.com.ar/'
 BODY_TEMPLATE_FILE = 'blog/template/body.html'
@@ -21,25 +25,39 @@ INDEX_TEMPLATE = Template(open('blog/template/index.html', 'r').read())
 DEBUG = True
 valid_title_chars = re.compile(r'[^a-zA-Z0-9._-]')
 
+@dataclass
+class PostMetadata:
+    title: str
+    tags: List[str]
+    description: str
+    date: date
+    incomplete: bool = False
+    path: Optional[str] = None
+
 def debug(*msg):
     if DEBUG:
         print(*msg)
 
 
-def parse_metadata(target):
+def parse_metadata(target) -> PostMetadata:
     data = open(target, 'r', encoding='utf-8').read()
     j = json.loads(data)
     j['date'] = datetime.strptime(j['date'], "%Y-%m-%d").date()
-    return j
+    return PostMetadata(**j)
 
 
-def generate_header(metadata):
+def generate_header(metadata: PostMetadata):
     template = Template('<h1>{{ title }}</h1><small>{{ date }}</small>')
-    return template.render(metadata)
+    return template.render(asdict(metadata))
 
 
-def generate_post(header, body, title, tags):
-    rendered = BODY_TEMPLATE.render(header=header, post=body, title=title, tags=tags, devmode=DEVMODE)
+def generate_post(header, body, title, tags, description):
+    rendered = BODY_TEMPLATE.render(header=header,
+            post=body,
+            title=title,
+            tags=tags,
+            description=description,
+            devmode=DEVMODE)
     return rendered
 
 
@@ -68,11 +86,11 @@ def main():
         debug(target)
 
         r = parse_metadata(metadata_file)
-        if 'incomplete' in r:
+        if r.incomplete:
             debug('Incomplete - skipping')
             continue
         header = generate_header(r)
-        safe_title = sanitize_title(r['title'])
+        safe_title = sanitize_title(r.title)
         html_fname = 'blog/html/%s.html' % safe_title
 
         if os.path.isfile(html_fname):
@@ -84,7 +102,7 @@ def main():
         md_str = open(post_file, encoding='utf-8').read()
         body_str = markdown2.markdown(md_str, extras=["fenced-code-blocks"])
         debug('generating text post')
-        html_str = generate_post(header, body_str, r['title'], r['tags'])
+        html_str = generate_post(header, body_str, r.title, r.tags, r.description)
         html = BeautifulSoup(html_str, features='html5lib')
         for header in html.find('article').find_all(["h1", "h2", "h3", "h4", "h5", "h6"]):
             header.attrs["id"] = header.text.lower().replace(' ', '-')
@@ -111,34 +129,34 @@ def generate_feed():
     return fg
 
 
-def make_rss_entry(feed, item):
+def make_rss_entry(feed, item: PostMetadata):
     fe = feed.add_entry()
-    url = '%s%s' % (BLOG_URL, item['path'][1:])
+    url = '%s%s' % (BLOG_URL, item.path[1:])
     fe.id(url)
-    tstamp = datetime.combine(item['date'], datetime.min.time())
+    tstamp = datetime.combine(item.date, datetime.min.time())
     tstamp = pytz.timezone("Europe/Amsterdam").localize(tstamp)
     fe.link(href=url)
     fe.author({'name': 'David Ventura',
                'email': 'davidventura27+blog@gmail.com'})
     fe.pubDate(tstamp)
-    fe.title(item['title'])
+    fe.title(item.title)
 #    fe.description(item['description'])
     # everything was mutated inside feed
     return tstamp
 
 
 def generate_index():
-    items = []
+    items: List[PostMetadata] = []
     feed = generate_feed()
     last_update = None
     for f in glob.glob("blog/raw/*/metadata.json"):
         item = parse_metadata(f)
-        if 'incomplete' in item and item['incomplete']:
+        if item.incomplete:
             continue
-        item['path'] = "/%s.html" % sanitize_title(item['title'])
+        item.path = "/%s.html" % sanitize_title(item.title)
         items.append(item)
 
-    s_items = sorted(items, key=lambda k: k['date'])
+    s_items = sorted(items, key=lambda k: k.date)
     for item in s_items:
         tstamp = make_rss_entry(feed, item)
         if last_update is None:
@@ -154,19 +172,19 @@ def get_all_tags():
     tags = set()
     for f in glob.glob("blog/raw/*/metadata.json"):
         item = parse_metadata(f)
-        tags = tags.union(set(item['tags']))
+        tags = tags.union(set(item.tags))
     return tags
 
 def generate_tag_index(tag):
-    items = []
+    items: List[PostMetadata] = []
     for f in glob.glob("blog/raw/*/metadata.json"):
         item = parse_metadata(f)
-        if tag not in item['tags']:
+        if tag not in item.tags:
             continue
-        item['path'] = "/%s.html" % sanitize_title(item['title'])
+        item.path = "/%s.html" % sanitize_title(item.title)
         items.append(item)
 
-    s_items = sorted(items, key=lambda k: k['date'], reverse=True)
+    s_items = sorted(items, key=lambda k: k.date, reverse=True)
     rendered = INDEX_TEMPLATE.render(index=s_items)
     fpath = Path('blog/html/tags/%s/index.html' % tag)
     fpath.parent.mkdir(parents=True, exist_ok=True)
