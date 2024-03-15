2ab84a55c39f99af295d1d48ed0d3bd4a017e337
David Ventura <davidventura27@gmail.com>
2024-03-15 19:24:13 +0100

    add messages

--- run.sh
+++ run.sh
@@ -5,4 +5,6 @@ venv/bin/python generate.py
 
 dot blog/raw/bookworm/architecture.dot -Tpng > blog/html/images/bookworm-architecture.png
 
+echo 'syncing'
 rsync -ar blog/html/ root@blog.davidv.dev:/var/www/blog-devops
+echo 'synced'
