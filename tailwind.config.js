02ba158bce287961bc61781e2f1c7ffefa992744
David Ventura <davidventura27@gmail.com>
2024-03-25 22:54:04 +0100

    lint

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,24 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["ui-sans-serif", "system-ui", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Helvetica Neue", "Arial", "Noto Sans", "sans-serif", "Segoe UI Symbol"]
+        sans: [
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+          "Noto Sans",
+          "sans-serif",
+          "Segoe UI Symbol",
+        ],
       },
       fontSize: {
-        base: ['1rem', '1.4']
-      }
+        base: ["1rem", "1.4"],
+      },
     },
   },
   plugins: [],
-}
+};
