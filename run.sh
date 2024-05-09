a6181a6671d82b879eecacfb7e31d3bdfc0cdff1
David Ventura <davidventura27@gmail.com>
2024-05-09 16:47:14 +0200

    add hint

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ set -e
 echo 'Generating..'
 venv/bin/python generate.py >/dev/null
 
-grep -r live.js blog/html/ && echo "Dev mode found in blogs, aborting" && exit 1
+grep -r live.js blog/html/ && echo -e "Dev mode found in blogs, aborting - run \ngrep -rl live.js blog/html/\n to check" && exit 1
 dot blog/raw/bookworm/architecture.dot -Tpng > blog/html/images/bookworm-architecture.png
 
 echo 'syncing'
