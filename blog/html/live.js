301adb878cfa0b35203ba269c190702ac311b5bf
David Ventura <davidventura27@gmail.com>
2024-12-22 18:49:45 +0100

    make live editor not do flashes of content

--- blog/html/live.js
+++ blog/html/live.js
@@ -1,13 +1,25 @@
 window.addEventListener("load", () => {
-  const now = new Date();
-  const check_and_reload = () => {
-    fetch(window.location.toString(), {method: 'HEAD', headers: {'If-Modified-Since': now.toGMTString()}}).then((response) => {
-      setTimeout(() => check_and_reload(), 200);
-      if(response.status === 304) {
-        return;
-      }
-      document.location.reload();
-    });
-  };
-  setTimeout(() => check_and_reload(), 200);
+    let now = new Date();
+    const check_and_reload = (when) => {
+        if (when !== undefined) {
+            now = when;
+        }
+
+        fetch(window.location.toString(), {method: 'HEAD', headers: {'If-Modified-Since': now.toGMTString()}}).then((response) => {
+            if(response.status === 304) {
+                setTimeout(() => check_and_reload(), 200);
+                return;
+            }
+            fetch(window.location.toString())
+                .then(response => response.text())
+                .then(text => {
+                    const parser = new DOMParser();
+                    const newDoc = parser.parseFromString(text, 'text/html');
+                    document.body.replaceWith(newDoc.body);
+                    console.log('replaced');
+                    setTimeout(() => check_and_reload(new Date()), 200);
+                });
+        });
+    };
+    setTimeout(() => check_and_reload(), 200);
 });
