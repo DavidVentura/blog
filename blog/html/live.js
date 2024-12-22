6d24dc56c6cbce506b996ab2e3f7f34ca439c88e
David Ventura <davidventura27@gmail.com>
2024-12-22 19:39:13 +0100

    improve live reloading a bit, but break image reloading

--- blog/html/live.js
+++ blog/html/live.js
@@ -1,3 +1,8 @@
+async function fresherThan(url, date) {
+    const response = await fetch(url, {method: 'HEAD', headers: {'If-Modified-Since': date.toGMTString()}});
+    return response.status !== 304;
+}
+
 window.addEventListener("load", () => {
     let now = new Date();
     const check_and_reload = (when) => {
@@ -6,18 +11,28 @@ window.addEventListener("load", () => {
         }
 
         fetch(window.location.toString(), {method: 'HEAD', headers: {'If-Modified-Since': now.toGMTString()}}).then((response) => {
+
             if(response.status === 304) {
-                setTimeout(() => check_and_reload(), 200);
+                setTimeout(() => check_and_reload(), 100);
                 return;
             }
+
             fetch(window.location.toString())
                 .then(response => response.text())
                 .then(text => {
                     const parser = new DOMParser();
                     const newDoc = parser.parseFromString(text, 'text/html');
+                    const position = document.scrollingElement.scrollTop;
                     document.body.replaceWith(newDoc.body);
-                    console.log('replaced');
-                    setTimeout(() => check_and_reload(new Date()), 200);
+                    /*
+                    [...document.body.querySelectorAll('img')].forEach(img => {
+                        img.src = `${img.src}?whatever=${now.toISOString()}`
+                    });
+                    setTimeout(() => {document.scrollingElement.scrollTop = position}, 50);
+                    causes FOUC SAD
+                    */
+                    
+                    setTimeout(() => check_and_reload(new Date()), 300);
                 });
         });
     };
